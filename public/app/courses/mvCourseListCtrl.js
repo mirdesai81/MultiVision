162bc7204b11f503a54523bb31c46041616de707
Mihir Desai <Mihir.Desai@retailsolutions.com>
2015-06-10 17:32:26 -0700

    Add filtering and Sorting on Courses

--- public/app/courses/mvCourseListCtrl.js
+++ public/app/courses/mvCourseListCtrl.js
@@ -3,4 +3,8 @@
  */
 angular.module('app').controller("mvCourseListCtrl",function($scope,mvCourse){
       $scope.courses = mvCourse.query();
+      $scope.sortOptions = [{value : 'title' , text : 'Sort By Title'},
+            {value : 'published' , text : 'Sort By Publish Date'}];
+
+      $scope.sortOrder = $scope.sortOptions[0].value;
 });
